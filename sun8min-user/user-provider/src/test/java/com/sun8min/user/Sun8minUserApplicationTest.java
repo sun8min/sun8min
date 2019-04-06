b4f0dd833dc1800282f883a273dd2c61103eef16
sun8min <sun8min@aliyun.com>
2019-04-06 23:04:53 +0800

    crud测试类修改

--- sun8min-user/user-provider/src/test/java/com/sun8min/user/Sun8minUserApplicationTest.java
+++ sun8min-user/user-provider/src/test/java/com/sun8min/user/Sun8minUserApplicationTest.java
@@ -1,10 +1,8 @@
 package com.sun8min.user;
 
-import com.sun8min.base.util.EnumUtils;
 import com.sun8min.user.entity.User;
 import com.sun8min.user.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.math.BigInteger;
-import java.util.List;
+import java.util.Optional;
 import java.util.Properties;
 
 @SpringBootTest
@@ -32,28 +29,13 @@ public class Sun8minUserApplicationTest {
     @Test
     @Transactional
     @Rollback
-    public void crud(){
-        userMapper.insert(new User().setUserNickName("AAA"));
-        userMapper.insert(new User().setUserNickName("BBB"));
-        userMapper.insert(new User().setUserNickName("CCC"));
-        userMapper.insert(new User().setUserNickName("DDD"));
-        userMapper.insert(new User().setUserNickName("EEE"));
-
-        List<User> users = userMapper.selectList(null);
-        User user = users.get(0);
-        BigInteger userId = user.getUserId();
-        log.info(EnumUtils.getEnumMsg(User.UserSex.class, user.getUserSex()));
-
-        user.setUserNickName("gay");
-        userMapper.updateById(user);
-        Assert.assertEquals(userMapper.selectById(userId).getUserNickName(), "gay");
-
-        userMapper.deleteById(userId);
+    public void crud() {
+        User user = userMapper.selectById(1);
+        Optional.ofNullable(user).map(User::getGmtCreate).ifPresent(time -> System.out.println(time));
     }
 
-
     @Test
-    public void testVersion(){
+    public void testVersion() {
         // 先尝试获取yaml，再获取properties，注意顺序，存在即使/src/main/resource/application.properties文件不存在，获取资源也不为空的情况
         String[] resourceNames = new String[]{"application.yml", "application.yaml", "application.properties"};
         Properties props = new Properties();
